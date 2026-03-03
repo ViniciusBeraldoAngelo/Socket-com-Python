f96a7f944c28e015b66fe4689399a3518c9bc3f4
Vinicius Beraldo Angelo <vinicius.beraldo.angelo@gmail.com>
2026-03-02 22:43:43 -0300

    server

--- servidor.py
+++ servidor.py
@@ -1,14 +1,43 @@
 import socket
+import threading
 
-HOST = '127.0.0.1'  # IP do servidor (localhost)
+HOST = '0.0.0.0'
 PORT = 5000
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect((HOST, PORT))
+clients = []
 
-client.sendall(b"Ola servidor!")
-data = client.recv(1024)
+def handle_client(conn, addr):
+    print(f"Novo cliente conectado: {addr}")
+    clients.append(conn)
 
-print("Resposta do servidor:", data.decode())
+    while True:
+        try:
+            msg = conn.recv(1024)
+            if not msg:
+                break
 
-client.close()
+            print(f"{addr}: {msg.decode()}")
+
+            # envia mensagem para todos os outros clientes
+            for client in clients:
+                if client != conn:
+                    client.sendall(msg)
+
+        except:
+            break
+
+    print(f"Cliente desconectado: {addr}")
+    clients.remove(conn)
+    conn.close()
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server.bind((HOST, PORT))
+server.listen()
+
+print("Servidor rodando...")
+
+while True:
+    conn, addr = server.accept()
+    thread = threading.Thread(target=handle_client, args=(conn, addr))
+    thread.start()
