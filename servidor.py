40b4494a8a72ee80f1af2b81da1d5344f8d8d9b1
Vinicius Beraldo Angelo <vinicius.beraldo.angelo@gmail.com>
2026-03-02 22:29:06 -0300

    Servidor

--- servidor.py
+++ servidor.py
@@ -1,1 +1,14 @@
-print ("Oi");
+import socket
+
+HOST = '127.0.0.1'  # IP do servidor (localhost)
+PORT = 5000
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((HOST, PORT))
+
+client.sendall(b"Ola servidor!")
+data = client.recv(1024)
+
+print("Resposta do servidor:", data.decode())
+
+client.close()
